68436a9308958a4e7d209fd1c5075717ded02faf
Daniel Eliasson <daniel@stilero.com>
2012-05-17 22:07:09 +0200

    Started working on a helper component

---
+++ helperComponent/admin/views/catcher/tmpl/default.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version     $Id$
+ * @copyright   Copyright 2011 Stilero AB. All rights re-served.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+//No direct access
+defined('_JEXEC) or die;');
+$code = JRequest::getVar('code');
+?>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            window.addEvent('domready', function(){
+                var code = $('catchcode').get('text');
+                window.opener.$('jform_params_auth_code').value = code;
+                window.opener.$('jform_params_auth_code').fireEvent('change');
+                window.close();
+            });
+        </script>
+    </head>
+    <body bgcolor="#FFFFFF">
+        <div id="catchcode"><?php echo $code; ?></div>
+    </body>
+</html>
+
---
+++ helperComponent/admin/views/catcher/view.html.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Description of instaModule
+ *
+ * @version  1.0
+ * @author Daniel Eliasson - joomla at stilero.com
+ * @copyright  (C) 2012-maj-17 Stilero Webdesign http://www.stilero.com
+ * @category Components
+ * @license	GPLv2
+ * 
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * 
+ * This file is part of view.html.
+ * 
+ * instaModule is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * instaModule is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with instaModule.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla view library
+jimport('joomla.application.component.view');
+ 
+/**
+ * HTML View class for the HelloWorld Component
+ */
+class InstagalleryViewCatcher extends JView
+{
+	// Overwriting JView display method
+	function display($tpl = null) 
+	{
+		// Assign data to the view
+		$this->msg = 'View.html';
+ 
+		// Display the view
+		parent::display($tpl);
+	}
+}
