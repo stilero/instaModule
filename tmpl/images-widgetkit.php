422395ee35aafe26a76c4d70ba677faeef3c1e73
Daniel Eliasson <daniel@stilero.com>
2012-06-24 10:13:41 +0200

    Added support for Widgetkit

--- tmpl/images-widgetkit.php
+++ tmpl/images-widgetkit.php
@@ -1,6 +1,11 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <div class="wk-gallery wk-gallery-wall clearfix polaroid ">
     <?php foreach ($images as $image) { ?>
-        <a class="" href="<?php echo $image['full'];?>" data-lightbox="group:25-4fa6cb3aaf15c" title="<?php echo $image['caption'] ?>"><div><img src="<?php echo $image['thumb'] ?>" alt="image1" height="150" width="150" /><p class="title"><?php echo $image['caption'] ?></p></div></a>
+        <a class="" href="<?php echo $image['full'];?>" data-lightbox="group:25-4fa6cb3aaf15c" title="<?php echo $image['caption'] ?>">
+            <div>
+                <img src="<?php echo $image['thumb'] ?>" alt="<?php echo $image['caption'] ?>" height="150" width="150" />
+                <p class="title"><?php echo $image['caption'] ?></p>
+            </div>
+        </a>
     <?php } ?>
 </div>
